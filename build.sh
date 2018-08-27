324dc35e2fef4a642cb8c67460b2c8a969ec9481
Olliver Schinagl <oliver@schinagl.nl>
2018-08-27 10:45:35 +0200

    Cleanup build env during start
    Some build artifacts are left behind after each build, so start by
    cleaning these up. This ensure stdeb can always run.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ WORKDIR="$(pwd)"
 MODULE_PATH="${WORKDIR}/inotify"
 
 cd "${MODULE_PATH}"
+rm -r deb_dist
+rm inotify-*.tar.gz
 python3 setup.py --command-packages=stdeb.command bdist_deb
 cp deb_dist/python3-inotify_*.deb "${WORKDIR}/"
